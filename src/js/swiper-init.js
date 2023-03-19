26f8c1f2450253a3121529fcbe68e03fa72d5f08
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-03-19 15:15:08 +0200

    add files

---
+++ src/js/swiper-init.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '</span>';
+    },
+  },
+  slidesPerView: 'auto',
+});

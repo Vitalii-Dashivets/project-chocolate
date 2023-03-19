b0009e79c6874c232205740e5d984fcc192dc3f4
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-03-19 21:10:47 +0200

    edit js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
  import "./js/mobile-menu.js"
 import "./js/modal.js"
 import "./js/swiper-init.js"
- import "./js/swiper.js"
+//  import "./js/swiper.js"
 // import "./js/swiper-bandle.js"
 // import "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"
